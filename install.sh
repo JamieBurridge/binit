5eb0bc949d1908861fc971c7eef6427fd8aac20d
JamieBurridge <xross_dev@proton.me>
2023-10-24 19:55:45 +0100

    add reinstall if script already installed

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@ install_dir="/usr/local/bin"
 
 chmod +x $script_name
 
-echo "Root permissions required to move to $install_dir"
+if sudo false; then
+	echo "Root permissions required to move to $install_dir"
+fi
+
+if [[ -e "$install_dir/$script_name" ]]; then
+	echo "You have ALREADY installed binit, reinstalling..."
+	rm "$install_dir/$script_name"
+fi
 
 sudo cp "$script_name" "$install_dir"
-echo "$script_name has been successfully installed to $install_dir"
+echo "$script_name has been successfully installed to \"$install_dir\""
